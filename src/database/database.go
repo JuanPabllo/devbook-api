505896367e28f82efdd1de4d121a7b9c71b17c8a
Juan Pablo <juanpablo.codes@gmail.com>
2022-09-17 22:57:37 -0300

    chore: create func database connect

---
+++ src/database/database.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"api/src/config"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Connect() (*sql.DB, error) {
+	db, erro := sql.Open("mysql", config.ConnectionStringDataBase)
+	if erro != nil {
+		return nil, erro
+	}
+
+	if erro = db.Ping(); erro != nil {
+		db.Close()
+		return nil, erro
+	}
+
+	return db, nil
+}
