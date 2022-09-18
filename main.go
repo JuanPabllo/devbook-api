53080a573438ddde2de6039932278b9af6717967
Juan Pablo <juanpablo.codes@gmail.com>
2022-09-17 22:49:00 -0300

    chore: initial config db and port

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -8,9 +9,10 @@ import (
 )
 
 func main() {
-	fmt.Println("Rodando api!")
+	config.Load()
+	fmt.Println("Server is running on port:", config.Port)
 
 	router := router.Generate()
 
-	log.Fatal(http.ListenAndServe(":5000", router))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), router))
 }
---
+++ src/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	ConnectionStringDataBase = ""
+	Port                     = 0
+)
+
+func Load() {
+	var erro error
+
+	if erro := godotenv.Load(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	Port, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		Port = 9000
+	}
+
+	ConnectionStringDataBase = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_NAME"))
+}
